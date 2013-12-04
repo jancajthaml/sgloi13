5ab6ec2fd97e4de4bd91eb97d2a139643aa69cb2
jancajthaml <jan.cajthaml@gmail.com>
2013-12-04 23:01:10 +0100

    minor fixes

--- source/cpp/core/FillPolygonModel.h
+++ source/cpp/core/FillPolygonModel.h
@@ -150,22 +150,47 @@ public:
 		Vertex s1		= ray.direction.crossProduct(cache_20);
 		float divisor	= s1*cache_10;
 
-		if( divisor==0.0f ) return false;
+		if( divisor==0.0f )
+		{
+			delete s1;
+			return false;
+		}
 
 		float inverse = 1.0f/divisor;
 
 		Vertex d	= ray.origin - vertices[0];
 		float b1	= ( d*s1) * inverse;
 
-		if( b1 < 0.0f || b1 > 1.0f ) return false;
+		if( b1 < 0.0f || b1 > 1.0f )
+		{
+			delete s1;
+			delete d;
+			return false;
+		}
 
 		Vertex s2	= d.crossProduct(cache_10);
 		float b2	= (ray.direction*s2) * inverse;
 
-		if( b2 < 0.0f || b1 + b2 > 1.0f ) return false;
+		if( b2 < 0.0f || b1 + b2 > 1.0f )
+		{
+			delete s1;
+			delete s2;
+			delete d;
+			return false;
+		}
 
 		float hit = (cache_20*s2) * inverse;
-		if( hit < 0.0f || hit > 1073741824.0f ) return false;
+		if( hit < 0.0f || hit > 1073741824.0f )
+		{
+			delete s1;
+			delete s2;
+			delete d;
+			return false;
+		}
+
+		delete s1;
+		delete s2;
+		delete d;
 
 		t = hit;
 		return true;
